e1d0507642e60cb62019e770b30b1a52f5fadb9a
matt_lu <lukas.mattenklodt@dlr.de>
2023-03-16 17:49:10 +0100

    initial commit

--- app/src/main/java/com/javetest/helio/MainActivity.java
+++ app/src/main/java/com/javetest/helio/MainActivity.java
@@ -2,13 +2,35 @@ package com.javetest.helio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonScan, buttonCompose, buttonKeyExchange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonScan = (Button) findViewById(R.id.button3);
+        buttonCompose = (Button) findViewById(R.id.button4);
+        buttonKeyExchange = (Button) findViewById(R.id.button5);
+
+        buttonScan.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
+
+//https://www.youtube.com/watch?v=drH63NpSWyk&t=22s
---
+++ app/src/main/java/com/javetest/helio/ScanActivity.java
@@ -0,0 +1,59 @@
+package com.javetest.helio;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class ScanActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+
+        //override the callback that is executed when a QR code is scanned and decoded
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                //this method is called after QR code is decoded -> decoded message in result
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //for now, show the decoded message as pop-up
+                        Toast.makeText(ScanActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+}
