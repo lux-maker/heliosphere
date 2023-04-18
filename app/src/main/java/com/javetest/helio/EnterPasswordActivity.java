8df0c76234735e39fd57a621539042aeba159327
AnanasClub24 <marius25092002@gmail.com>
2023-04-18 23:49:48 +0200

    Error fixed when Enter was pressed but no password had been entered at the start

--- app/src/main/java/com/javetest/helio/EnterPasswordActivity.java
+++ app/src/main/java/com/javetest/helio/EnterPasswordActivity.java
@@ -24,7 +24,7 @@ import javax.crypto.spec.SecretKeySpec;
  * und dieses nun Überprüft werden soll.
  * Dazu wird der Hash-Wert des APP-PWs aus den (encrypted) shared Preferences geladen, sowie der verwendete Salt. Mit dem Salt wird das zu überprüfende PW gehashed und mit dem Hash-Wert des App-PWs verglichen.
  * Wenn die PWs gleich sind, wird die Main Activity gestartet, ansonsten wird eine Fehlermeldung angezeigt.
- * //TODO APP stürtzt ab, wenn kein PW einegeben wird, aber Enter gedrückt wird
+ * 
  */
 public class EnterPasswordActivity extends AppCompatActivity {
 
@@ -47,38 +47,42 @@ public class EnterPasswordActivity extends AppCompatActivity {
             public void onClick(View view) //when "ENTER" button clicked: speichere die Eingabe, lade den Hash-Wert und den Salt des APP-PWs, hashe das eingegebene PW mit dem geladenen Salt und vergleiche die Hash-Werte. Wenn gleich, dann starte Main Activity sonst zeige eine Fehlermeldung an.
             {
                 String p = enteredPW.getText().toString();
-
-                //load password from memory
-                MasterKey masterKey = EncryptedSharedPreferencesHandler.getMasterKey(getApplicationContext());
-                SharedPreferences settings = EncryptedSharedPreferencesHandler.getESP(getApplicationContext(), masterKey, "AccessKey");
-
-                //reorganize the hashed password from memory
-                Gson gson = new Gson();
-                String json = settings.getString("hashedPWInfo", ""); //TODO sichergehen dass an dieser stelle niemals "" zurückgegeben wird
-
-                //json wurde in CreatePasswort Activity aufwendig aufgeteilt und verknüpft, dass müssen wir jetzt wieder entdrosseln:
-                String[] json_split = json.split(";"); //json = "salt ; hash ; algorithm"
-                byte[] pw_salt = gson.fromJson(json_split[0], byte[].class);
-                byte[] pw_SecretKeySpec_key = gson.fromJson(json_split[1], byte[].class);
-                String pw_SecretKeySpec_algorithm = gson.fromJson(json_split[2], String.class);
-                SecretKeySpec secretKeySpec = new SecretKeySpec(pw_SecretKeySpec_key, pw_SecretKeySpec_algorithm); //erstelle ein secretKeySpec Objekt, damit wir das für die HashedPasswordInfo class verwenden können
-                HashedPasswordInfo trueHashedPasswordInfo = new HashedPasswordInfo(pw_salt, secretKeySpec); //Objekt enthält Hash-Wert des App-PWs und den Salt
-
-
-                //hash the newly entered password by reusing the salt from the trueHashedPassword
-                HashedPasswordInfo hashedPasswordInfo = HelperFunctionsCrypto.hashPassword(trueHashedPasswordInfo.getSalt(), p.toCharArray());
-
-                // compare hashes with overwritten equals ("==") operator
-                if(hashedPasswordInfo.equals(trueHashedPasswordInfo)) //hashedPasswordInfo = eingegebenes PW // trueHashedPasswordInfo = tatsächliches PW-Passwort
+                if(p.equals(""))
                 {
-                    //start the main application
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                    Toast.makeText(EnterPasswordActivity.this, "please type in password", Toast.LENGTH_SHORT).show();
                 }
-                else
-                {
-                    Toast.makeText(EnterPasswordActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                else {
+
+                    //load password from memory
+                    MasterKey masterKey = EncryptedSharedPreferencesHandler.getMasterKey(getApplicationContext());
+                    SharedPreferences settings = EncryptedSharedPreferencesHandler.getESP(getApplicationContext(), masterKey, "AccessKey");
+
+                    //reorganize the hashed password from memory
+                    Gson gson = new Gson();
+                    String json = settings.getString("hashedPWInfo", ""); //TODO sichergehen dass an dieser stelle niemals "" zurückgegeben wird
+
+                    //json wurde in CreatePasswort Activity aufwendig aufgeteilt und verknüpft, dass müssen wir jetzt wieder entdrosseln:
+                    String[] json_split = json.split(";"); //json = "salt ; hash ; algorithm"
+                    byte[] pw_salt = gson.fromJson(json_split[0], byte[].class);
+                    byte[] pw_SecretKeySpec_key = gson.fromJson(json_split[1], byte[].class);
+                    String pw_SecretKeySpec_algorithm = gson.fromJson(json_split[2], String.class);
+                    SecretKeySpec secretKeySpec = new SecretKeySpec(pw_SecretKeySpec_key, pw_SecretKeySpec_algorithm); //erstelle ein secretKeySpec Objekt, damit wir das für die HashedPasswordInfo class verwenden können
+                    HashedPasswordInfo trueHashedPasswordInfo = new HashedPasswordInfo(pw_salt, secretKeySpec); //Objekt enthält Hash-Wert des App-PWs und den Salt
+
+
+                    //hash the newly entered password by reusing the salt from the trueHashedPassword
+                    HashedPasswordInfo hashedPasswordInfo = HelperFunctionsCrypto.hashPassword(trueHashedPasswordInfo.getSalt(), p.toCharArray());
+
+                    // compare hashes with overwritten equals ("==") operator
+                    if (hashedPasswordInfo.equals(trueHashedPasswordInfo)) //hashedPasswordInfo = eingegebenes PW // trueHashedPasswordInfo = tatsächliches PW-Passwort
+                    {
+                        //start the main application
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(EnterPasswordActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
