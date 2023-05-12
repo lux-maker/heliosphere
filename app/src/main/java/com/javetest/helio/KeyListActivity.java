00afa9cbaa8a21156bb2113b81005cab2ff64a58
matt_lu <lukas.mattenklodt@dlr.de>
2023-05-13 00:49:48 +0200

    integrated panic button to delete everything

--- app/src/main/java/com/javetest/helio/KeyListActivity.java
+++ app/src/main/java/com/javetest/helio/KeyListActivity.java
@@ -206,7 +206,6 @@ public class KeyListActivity extends AppCompatActivity {
         });
     }
 
-
     /**
      * erzeugt einen QR Code mit der übergebenen Nachricht und zeigt ihn an
      * @param qrindex Zahl, die anzeigt welcher QR Code anzeiget werden soll
